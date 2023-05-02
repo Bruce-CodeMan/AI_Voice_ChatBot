b6bc8254334badbd792cc55685128bfd6aa0667b
Bruce <1033684650@qq.com>
2023-05-02 11:14:25 +0800

    update the controller.tsx

--- frontend_react/src/components/BetBox.tsx
+++ frontend_react/src/components/BetBox.tsx
@@ -3,12 +3,15 @@ import BetButton from "./BetButton";
 interface IProps {
     betDirection: string;
     setBetDirection: any;
+    isLoading: boolean;
 }
 
-function BetBox({ betDirection, setBetDirection }: IProps) {
+function BetBox({ betDirection, setBetDirection, isLoading }: IProps) {
     return (
         <div className="mt-5">
-            <div className="py-2 border bg-red-100">Layer 1</div>
+            <div className="py-2 border bg-gray-700 text-white text-right pr-12">
+                { isLoading?"Loading...": "Result" }
+            </div>
             <div className="flex flex-row justify-between py-2 border bg-pink-100">
                 <BetButton 
                     direction="down" 
--- frontend_react/src/components/Controller.tsx
+++ frontend_react/src/components/Controller.tsx
@@ -1,24 +1,50 @@
 import Button from "./Button";
 import { useState, useEffect } from "react";
 import BetBox from "./BetBox";
+import axios from "axios";
 
 function Controller() {
 
-    const [ show, setShow ] = useState(false);
+    const [ hasWon, setHasWon ] = useState(false);
     const [ betDirection, setBetDirection ] = useState("up");
+    const [ isLoading, setIsLoading ] = useState(false);
 
-    const containerHandler = () => {
-        setShow(!show);
+    const containerHandler = async () => {
+
+        setIsLoading(true);
+
+        let isShow = false;
+        const url = "https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new"
+        await axios
+                .get(url)
+                .then((res) => {
+                if(res.status == 200){
+                    const val = res.data;
+                    console.log(val)
+                    if(val >= 50 && betDirection == "up") {
+                        isShow = true;
+                    }else if(val < 50 && betDirection == "down") {
+                        isShow = false;
+                    }
+                }else{
+                    console.log('出错啦！！！！')
+                }
+            }).catch((err) => {
+                console.log(err.data, err.message);
+            });
+        // Return winner result
+        setHasWon(isShow);
+        setIsLoading(false);
     }
 
     useEffect(() => {
-        console.log(betDirection)
-    }, [betDirection])
+        console.log(hasWon)
+    }, [hasWon])
 
     return (
         <div className=" w-full md:w-[850px] lg:w-[1200px] bg-green-500 py-12 container mx-auto px-5">
             <Button runHandler={containerHandler}/>
-            <BetBox betDirection={betDirection} setBetDirection={setBetDirection}/>
+            <BetBox betDirection={betDirection} setBetDirection={setBetDirection} isLoading={isLoading}/>
         </div>
     )
 }
